65b9174b502768644d1c91db141b1e6770072a90
spencer-mcguire <smcguire1826@gmail.com>
2019-11-15 13:02:07 -0600

    fix axios

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -5,16 +5,25 @@ import LocationCard from "./LocationCard";
 const LocationsList = () => {
   const [data, setData] = useState([]);
   console.log(data);
+
   useEffect(() => {
     axios
       .get("https://rickandmortyapi.com/api/location/")
       .then(res => {
-        setData(res);
+        setData(res.results);
       })
       .catch(err => console.log(err));
   }, []);
 
-  return <LocationCard />;
+  return (
+    <section>
+      <div className="character-list">
+        {data.map(i => (
+          <LocationCard key={i.id} i={i} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default LocationsList;
