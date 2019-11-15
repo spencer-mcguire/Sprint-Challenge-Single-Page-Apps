552b12419ce82ae311163db1ff47fd4a6fc6846f
spencer-mcguire <smcguire1826@gmail.com>
2019-11-15 11:31:31 -0600

    home page style

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,9 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 
 const WelcomePage = props => {
+  const Button = styled.button`
+    padding: 15px 50px;
+    border: none;
+    border-radius: 4px;
+    background: radial-gradient(circle, #3f5efb, #46fafc);
+    color: white;
+    font-weight: bold;
+    font-size: 1.5rem;
+    box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 10px 7px rgba(0, 0, 0, 0.22);
+  `;
+
   const routeToChar = () => {
     props.history.push("/characters");
   };
+
   return (
     <section className="welcome-page">
       <header>
@@ -14,7 +27,9 @@ const WelcomePage = props => {
           alt="rick"
         />
       </header>
-      <button onClick={routeToChar}>Checkout the Characters</button>
+      <div className="home-button">
+        <Button onClick={routeToChar}>Checkout the Characters</Button>
+      </div>
     </section>
   );
 };
